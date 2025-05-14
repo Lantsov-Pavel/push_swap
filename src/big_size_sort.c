c70b7be3ded781573d67f74a7e6126f9933f42f4
plantsov <plantsov@student.42berlin.de>
2025-05-14 14:20:13 +0200

    refactoring

--- src/big_size_sort.c
+++ src/big_size_sort.c
@@ -1,27 +1,27 @@
 #include "../includes/push_swap.h"
 
-static void	put_nodes_back(t_node **stack_a, t_node **stack_b)
+static void	put_nodes_back(t_node **a_stack, t_node **b_stack)
 {
-	while (*stack_b)
-		push_a(stack_a, stack_b);
+	while (*b_stack)
+		push_a(a_stack, b_stack);
 }
 
-static void	sort_condition(t_node **stack_a, t_node **stack_b, int i)
+static void	sort_condition(t_node **a_stack, t_node **b_stack, int i)
 {
-	if ((((*stack_a)->index >> i) & 1) == 0)
-		push_b(stack_a, stack_b);
+	if ((((*a_stack)->index >> i) & 1) == 0)
+		push_b(a_stack, b_stack);
 	else
-		rotate_a(stack_a);
+		rotate_a(a_stack);
 }
 
-void	radix_sort(t_node **stack_a, t_node **stack_b)
+void	radix_sort(t_node **a_stack, t_node **b_stack)
 {
 	int	size;
 	int	max_bits;
 	int	i;
 	int	j;
 
-	size = stack_size(*stack_a);
+	size = stack_size(*a_stack);
 	max_bits = 0;
 	while ((size - 1) >> max_bits)
 		max_bits++;
@@ -31,10 +31,10 @@ void	radix_sort(t_node **stack_a, t_node **stack_b)
 		j = 0;
 		while (j < size)
 		{
-			sort_condition(stack_a, stack_b, i);
+			sort_condition(a_stack, b_stack, i);
 			j++;
 		}
-		put_nodes_back(stack_a, stack_b);
+		put_nodes_back(a_stack, b_stack);
 		i++;
 	}
 }
--- src/mid_size_sort.c
+++ src/mid_size_sort.c
@@ -24,48 +24,48 @@ static int	find_minimal_position(t_node *stack)
 	return (position);
 }
 
-static void	put_on_top_min(int min_position, t_node **stack_a)
+static void	put_on_top_min(int min_position, t_node **a_stack)
 {
 	int	size;
 
-	size = stack_size(*stack_a);
+	size = stack_size(*a_stack);
 	if (min_position == 0)
 		return ;
 	else if (min_position == 1)
-		rotate_a(stack_a);
+		rotate_a(a_stack);
 	else if (min_position == 2)
 	{
-		rotate_a(stack_a);
-		rotate_a(stack_a);
+		rotate_a(a_stack);
+		rotate_a(a_stack);
 	}
 	else if (min_position == 3)
 	{
-		reverse_rotate_a(stack_a);
+		reverse_rotate_a(a_stack);
 		if (size == 5)
-			reverse_rotate_a(stack_a);
+			reverse_rotate_a(a_stack);
 	}
 	else if (min_position == 4)
-		reverse_rotate_a(stack_a);
+		reverse_rotate_a(a_stack);
 }
 
-void	sort_four_nodes(t_node **stack_a, t_node **stack_b)
+void	sort_four_nodes(t_node **a_stack, t_node **b_stack)
 {
 	int	min_position;
 
-	min_position = find_minimal_position((*stack_a));
-	put_on_top_min(min_position, stack_a);
-	push_b(stack_a, stack_b);
-	sort_three_nodes(stack_a);
-	push_a(stack_a, stack_b);
+	min_position = find_minimal_position((*a_stack));
+	put_on_top_min(min_position, a_stack);
+	push_b(a_stack, b_stack);
+	sort_three_nodes(a_stack);
+	push_a(a_stack, b_stack);
 }
 
-void	sort_five_nodes(t_node **stack_a, t_node **stack_b)
+void	sort_five_nodes(t_node **a_stack, t_node **b_stack)
 {
 	int	first_min_position;
 
-	first_min_position = find_minimal_position(*stack_a);
-	put_on_top_min(first_min_position, stack_a);
-	push_b(stack_a, stack_b);
-	sort_four_nodes(stack_a, stack_b);
-	push_a(stack_a, stack_b);
+	first_min_position = find_minimal_position(*b_stack);
+	put_on_top_min(first_min_position, a_stack);
+	push_b(a_stack, b_stack);
+	sort_four_nodes(a_stack, b_stack);
+	push_a(a_stack, b_stack);
 }
